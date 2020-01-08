243120536fb2d950df77f6e08ba29631c06fd199
wangxizzz <979675820@qq.com>
2020-01-08 20:39:39 +0800

    创建新工程

---
+++ j2se/src/main/java/com/example/j2se/J2seApplication.java
@@ -0,0 +1,13 @@
+package com.example.j2se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2seApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2seApplication.class, args);
+    }
+
+}
---
+++ j2se/src/test/java/com/example/j2se/J2seApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j2se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2seApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
