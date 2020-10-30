ad2fa3f6f3985986f39803315135702900025dd7
wangxi <wangxi03@koolearn-inc.com>
2020-10-30 16:48:45 +0800

    mainTest

---
+++ framework/src/test/java/com/example/framework/MainTest.java
@@ -0,0 +1,15 @@
+package com.example.framework;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author wangxi03 created on 2020/10/30 11:39 AM
+ * @version v1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainTest {
+
+}
